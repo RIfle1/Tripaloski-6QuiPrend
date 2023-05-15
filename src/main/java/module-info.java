783b06b46cca941c2cc37b71b3e3f03b2b92e0c5
Rifle1 <philipe.barakat@yahoo.com>
2023-05-15 18:53:08 +0200

    updt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module project.tripaloski6quiprend {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens project.tripaloski6quiprend to javafx.fxml;
+    exports project.tripaloski6quiprend;
+}
